a13dedb012184cdd8cb50f35d7541c769a15a2e3
phuonglili <80459864+phuonglili@users.noreply.github.com>
2021-11-04 21:52:24 +0700

    update data

--- teacher-homework/delete.php
+++ teacher-homework/delete.php
@@ -1,6 +1,6 @@
 <?php
 
-include('config/constants.php');
+include('templates/config.php');
 $sql = "delete from give_hw where id_hw = '$_GET[id_hw]'";
 		mysqli_query($conn,$sql);
 		header('location:exercise.php?id_hw=');
