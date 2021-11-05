ebcaa3554becacb00c31bfc561be319135f20330
NgoHoangThuy <90167819+NgoHoangThuy@users.noreply.github.com>
2021-11-06 00:13:09 +0700

    update

--- student/submit.php
+++ student/submit.php
@@ -21,6 +21,45 @@ if($db->connect_error){
 <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 </head>
 <body>
+<div class="header " style="height: 100px">
+                <div class="container-fluid" >
+                <div class="row">
+                    <div class="col-md-12">
+                        <div class="row">
+                            <div class="col-md-2 img-fluid">
+                                <img src="img/logo.png" alt="" style="width:180px; height:80px; margin-top:15px">
+                            </div>
+                            <div class="col-md-8">
+                                
+                            </div>
+                            <div class="col-md-2">
+                            <div class="container-fluid">
+                                <div class="row">
+                                    <div class="col-md-12">
+                                        <div class="dropdown">
+                                            
+                                            <button style="margin:40px 0px 0px 50px  ;background-color:  #f2f4f7;border: 1px solid hsla(0,0%,86.3%,.8); border-radius: 8px;  cursor: pointer; padding: 12px; "class="btn  dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown">
+                                
+                                                <i class="fas fa-users"></i>
+                                            
+                                                
+                                            </button>
+                                            <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+                                                <a class="dropdown-item " href="">Về trang chủ</a>
+                                                 <a class="dropdown-item" href="">Đăng xuất</a> 
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            </div>
+                        </div>
+                    </div>
+                    </div>
+                </div>
+            </div>
+      
+
 <div class="container">
 <div class="row">
 
@@ -33,17 +72,17 @@ if($db->connect_error){
 
 <div class="form-group">
 <label for="masv">Mã sinh viên</label>
-<input type="text" class="form-control" id="masv" name="masv" placeholder="Enter masv" required />
+<input type="text" class="form-control" id="masv" name="masv" placeholder="Nhập mã sinh viên" required />
 </div>
 
 <div class="form-group">
 <label for="mabt">Mã bài tập</label>
-<input type="text" class="form-control" id="mabt" name="mabt" placeholder="Enter mabt" required />
+<input type="text" class="form-control" id="mabt" name="mabt" placeholder="Nhập mã bài tập" required />
 </div>
 
-<input style="margin-top:50px" id="uploadfile" type="file" accept="file/*" name="file" />
+<input id="uploadImage" type="file" accept="image/*" name="image" />
 
-<input class="btn btn-success" type="submit" value="NỘP BÀI">
+<input style="margin-top:50px " class="btn btn-success" type="submit" value="NỘP BÀI">
 </form>
 
 <div id="err"></div>
@@ -51,6 +90,7 @@ if($db->connect_error){
 </div>
 </div>
 </div>
+
 
 <div class="footer" style="height:300px;background-color:  #3c5ca5; margin-top:50px">
                 <div class="container">
@@ -69,6 +109,8 @@ if($db->connect_error){
                     </p>
                 </div>
             </div>
+
+
 </body>
 
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
